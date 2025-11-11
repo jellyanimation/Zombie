d4ad41d62eccb1ed80d56d024167aaece1e1732a
jellyanimation <gjanimation53@gmail.com>
2025-11-11 10:50:16 +0900

    202511111050

--- Assets/01.Scripts/UIManager.cs
+++ Assets/01.Scripts/UIManager.cs
@@ -11,7 +11,7 @@ public class UIManager : MonoBehaviour {
         {
             if (m_instance == null)
             {
-                m_instance = FindObjectOfType<UIManager>();
+                m_instance = FindFirstObjectByType<UIManager>();
             }
 
             return m_instance;
@@ -24,6 +24,16 @@ public class UIManager : MonoBehaviour {
     public Text scoreText; // 점수 표시용 텍스트
     public Text waveText; // 적 웨이브 표시용 텍스트
     public GameObject gameoverUI; // 게임 오버시 활성화할 UI 
+
+
+    private void Awake()
+    {
+        if(instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
 
     // 탄약 텍스트 갱신
     public void UpdateAmmoText(int magAmmo, int remainAmmo) {
